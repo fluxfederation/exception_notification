f0f0b54d2de5b244ec934623e10e792bc9c08651
Will Bryant <will.bryant@gmail.com>
2014-01-15 23:16:04 +1300

    B#46280 filter the env vars using a whitelist

--- lib/exception_notifier/notifier.rb
+++ lib/exception_notifier/notifier.rb
@@ -6,6 +6,44 @@ class ExceptionNotifier
     self.mailer_name = 'exception_notifier'
     self.append_view_path "#{File.dirname(__FILE__)}/views"
 
+    @@whitelisted_env_vars = [
+      'action_dispatch.request.parameters',
+      'action_dispatch.request.path_parameters',
+      'action_dispatch.request.query_parameters',
+      'action_dispatch.request.request_parameters',
+      'BUNDLE_BIN_PATH',
+      'BUNDLE_GEMFILE',
+      'CONTENT_LENGTH',
+      'CONTENT_TYPE',
+      'DOCUMENT_ROOT',
+      'GEM_HOME',
+      'HOME',
+      /HTTP_/,
+      'ORIGINAL_FULLPATH',
+      'PASSENGER_APP_TYPE',
+      'PASSENGER_ENV',
+      'PASSENGER_RUBY',
+      'PASSENGER_SPAWN_METHOD',
+      'PASSENGER_USER',
+      'PATH',
+      'PATH_INFO',
+      'PWD',
+      'RAILS_ENV',
+      'REMOTE_ADDR',
+      'REMOTE_PORT',
+      'REQUEST_METHOD',
+      'REQUEST_URI',
+      'RUBYOPT',
+      'SERVER_ADDR',
+      'SERVER_NAME',
+      'SERVER_PORT',
+      'SERVER_PROTOCOL',
+      'SERVER_SOFTWARE',
+      'TMPDIR',
+      'USER',
+    ]
+    cattr_accessor :whitelisted_env_vars
+
     class << self
       def default_sender_address
         %("Exception Notifier" <exception.notifier@default.com>)
@@ -37,11 +75,13 @@ class ExceptionNotifier
     end
 
     def exception_notification(env, exception)
-      @env        = env
       @exception  = exception
+      @parameter_filter = ActionDispatch::Http::ParameterFilter.new(env["action_dispatch.parameter_filter"])
+      @request    = ActionDispatch::Request.new(env)
+      @env        = whitelist_env(@request.try(:filtered_env) || @parameter_filter.filter(env))
+      @session    = @parameter_filter.filter(@request.session)
       @options    = (env['exception_notifier.options'] || {}).reverse_merge(self.class.default_options)
       @kontroller = env['action_controller.instance'] || MissingController.new
-      @request    = ActionDispatch::Request.new(env)
       @backtrace  = clean_backtrace(exception)
       @sections   = @options[:sections]
       @source     = "#{@kontroller.controller_name}##{@kontroller.action_name}"
@@ -59,6 +99,14 @@ class ExceptionNotifier
     end
 
     private
+
+      def whitelist_env(env)
+        env.select do |key, val|
+          whitelisted_env_vars.any? do |allowed|
+            allowed.is_a?(Regexp) ? key =~ allowed : key == allowed
+          end
+        end
+      end
       
       def clean_backtrace(exception)
         Rails.respond_to?(:backtrace_cleaner) ?
